cbdeaf8a6603ff48da1926a7a0cf18b59917064a
Simon <lishi1608@gmail.com>
2018-10-20 00:55:12 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+import 'bulma'
+// ================================
+// START YOUR APP HERE
+// ================================
---
+++ webpack.config.js
@@ -0,0 +1,94 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Is the current build a development build
+const IS_DEV = (process.env.NODE_ENV === 'dev');
+
+const dirApp = path.join(__dirname, 'app');
+const dirAssets = path.join(__dirname, 'assets');
+
+const appHtmlTitle = 'TEDxXujiahui';
+
+/**
+ * Webpack Configuration
+ */
+module.exports = {
+    entry: {
+        bundle: path.join(dirApp, 'index')
+    },
+    resolve: { // change where webpack goes to look for imported module
+        modules: [ // Tell webpack what directories should be searched when resolving modules
+            'node_modules',
+            dirApp,
+            dirAssets
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            IS_DEV: IS_DEV
+        }),
+
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.ejs'),
+            title: appHtmlTitle
+        })
+    ],
+    module: {
+        rules: [
+            // BABEL
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    compact: true
+                }
+            },
+
+            // STYLES
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: IS_DEV
+                        }
+                    },
+                ]
+            },
+
+            // CSS / SASS
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                    'style-loader', // Adds CSS to the DOM by injecting a <style> tag
+                    {
+                        loader: 'css-loader', // The css-loader interprets @import and url() like import/require() and will resolve them.
+                        options: {
+                            sourceMap: IS_DEV
+                        }
+                    },
+                    {
+                        loader: 'sass-loader', // Loads a Sass/SCSS file and compiles it to CSS
+                        options: {
+                            sourceMap: IS_DEV,
+                            includePaths: [dirAssets]
+                        }
+                    }
+                ]
+            },
+
+            // IMAGES
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }
+        ]
+    }
+};
