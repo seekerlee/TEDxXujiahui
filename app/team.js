0b35f20134a0a4301c2f2b1284cf1b2a16145ce5
Simon <lishi1608@gmail.com>
2018-11-13 21:25:36 +0800

    add page

---
+++ app/team.js
@@ -0,0 +1,20 @@
+
+import 'styles/common.scss'
+import 'animate.css'
+import '@mdi/font/css/materialdesignicons.css'
+import 'styles/team.scss'
+
+import './nav'
+import Siema from 'siema';
+const siema = new Siema({
+    perPage: {
+      768: 1,
+      800: 3,
+      1024: 4,
+      1366: 5
+    },
+    loop: true
+});
+setInterval(()=>{
+    siema.next();
+}, 2000)
